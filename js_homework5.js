657e9c463b3e987c5fb2e8c3c0e81fff91b0d6ce
Timuravko <murchi22236@gmail.com>
2023-12-07 10:40:08 +0200

    js_homework5

---
+++ js_homework5.js
@@ -0,0 +1,27 @@
+// // 1.
+// var a = 10;
+// if (isNaN(a)) {
+//     console.log(true)
+// }
+// else {
+//     console.log(false)
+// }
+
+// 2.
+// var a = 'text';
+// var b = new Boolean(true);
+// function pad(a, b) {
+//     if (b.valueOf = true) {
+//         console.log(a.padStart(5, '*'));
+//     } else if (b.valueOf = false) {
+//         console.log(a.padEnd(5, '*'))
+//     }
+// }
+// pad(a, b)
+
+// // 3.
+// var rnd = Math.random();
+// rnd *= 1000;
+// rnd = Math.round(rnd);
+// rnd >= 100;
+// console.log(rnd);
